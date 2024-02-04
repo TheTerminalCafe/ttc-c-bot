ea00963c386d6cf9d826480870c32fe5c6cedc79
Caitcatdev <caitcatdev@protonmail.com>
2024-02-04 20:20:47 +0100

    deleted old file

--- src/self_roles.c
+++
@@ -1,9 +0,0 @@
-#include "ttc-discord/gateway.h"
-#include <ttc-discord/api.h>
-#include <ttc-discord/discord.h>
-#include <ttc-discord/interaction.h>
-
-int ttc_bot_edit_echo(ttc_discord_interaction_t *interaction, snowflake_t cid, snowflake_t mid) {
-	const char *fmt = "/channels/%lu/messages/%lu";
-	char *message_url;
-}
